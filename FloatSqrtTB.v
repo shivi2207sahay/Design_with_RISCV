99858dcbab100c158209e6c393dcc7d30bc94c6c
shivi2207sahay <141152904+shivi2207sahay@users.noreply.github.com>
2023-11-06 13:06:35 +0530

    Update FloatSqrtTB.v

--- FloatSqrtTB.v
+++ FloatSqrtTB.v
@@ -8,30 +8,15 @@ FloatingSqrt F_Mult (.A(A),.result(result));
 
 initial  
 begin
-A = 32'h41c80000;  // 25
+A = 32'h41c80000; 
 #20
-A = 32'h42040000;  // 33
+A = 32'h42040000; 
 #20
-A = 32'h42aa0000;  // 85
+A = 32'h42aa0000;  
 #20
-A = 32'h42b80000;  // 92
+A = 32'h42b80000; 
 end
 
-initial
-begin
-#15
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",5.0,value);
-#20
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",5.744562646538029,value);
-#20
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",9.219544457292887,value);
-#20
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",9.591663046625438,value);
-$finish;
-end
+
 
 endmodule
